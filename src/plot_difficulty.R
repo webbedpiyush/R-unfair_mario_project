4000035008743ace3a563f471cca49fcdaff95fe
Piyush Tiwari <125976928+webbedpiyush@users.noreply.github.com>
2023-11-26 17:48:33 +0530

    Updated the code

--- src/plot_difficulty.R
+++ src/plot_difficulty.R
@@ -1,1 +1,16 @@
+# plot_difficulty.R
+
+# Plotting the Unfair Mario level difficulty profile
+ggplot(unfair_mario_difficulty, aes(x = level, y = p_win)) +
+  geom_line() +
+  scale_x_continuous(breaks = c(1:15)) +
+  scale_y_continuous(labels = scales::percent)
+
+# Adding points and a dashed line
+ggplot(unfair_mario_difficulty, aes(x = level, y = p_win)) +
+  geom_line() +
+  geom_point() +
+  geom_hline(yintercept = 0.10, linetype = "dashed") +
+  scale_x_continuous(breaks = c(1:15)) +
+  scale_y_continuous(labels = scales::percent)
  
