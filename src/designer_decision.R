2a765f3b42c2eee29416ec2df1a006dd8be0fc1d
Piyush Tiwari <125976928+webbedpiyush@users.noreply.github.com>
2023-11-26 17:49:49 +0530

    Updated the code ( As if the designer would feel worry about players)

--- src/designer_decision.R
+++ src/designer_decision.R
@@ -1,1 +1,6 @@
+# designer_decision.R
+
+# Should our level designer worry about that a lot of 
+# players will complete the episode in one attempt for Unfair Mario?
+should_the_unfair_mario_designer_worry = FALSE
  
