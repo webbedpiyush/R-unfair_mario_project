fbdea35c5d4b59acf90bf7c1c88c124aa0b1f136
Piyush Tiwari <125976928+webbedpiyush@users.noreply.github.com>
2023-11-26 17:47:27 +0530

    Updated the code

--- src/load_and_explore_data.r
+++ src/load_and_explore_data.r
@@ -1,1 +1,18 @@
- 
+# load_and_explore_data.R
+
+# This sets the size of plots to a good default.
+options(repr.plot.width = 5, repr.plot.height = 4)
+
+# Loading in packages
+library(tidyverse)
+
+# Reading in the data for Unfair Mario
+unfair_mario_data <- read_csv("datasets/unfair_mario.csv")
+
+# Printing out the first couple of rows
+head(unfair_mario_data)
+print("Number of players:")
+length(unique(unfair_mario_data$player_id))
+
+print("Period for which we have data:")
+range(unfair_mario_data$dt) 
