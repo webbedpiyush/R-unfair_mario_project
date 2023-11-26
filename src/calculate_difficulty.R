7890ff51912493b24d732f5b4590ed3c02587e72
Piyush Tiwari <125976928+webbedpiyush@users.noreply.github.com>
2023-11-26 17:48:08 +0530

    Update the code

--- src/calculate_difficulty.R
+++ src/calculate_difficulty.R
@@ -1,1 +1,11 @@
+# calculate_difficulty.R
+
+# Calculating level difficulty for Unfair Mario
+unfair_mario_difficulty <- unfair_mario_data %>%
+  group_by(level) %>%
+  summarise(wins = sum(num_success), attempts = sum(num_attempts)) %>%
+  mutate(p_win = wins / attempts)
+
+# Printing out the Unfair Mario level difficulty
+unfair_mario_difficulty
  
